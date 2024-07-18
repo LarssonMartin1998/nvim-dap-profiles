6edc4c9c44282ff843e56585eb0da52184bfe646
Martin Larsson <larssonmartin1998@gmail.com>
2024-07-18 12:46:31 +0200

    Add profiles, create/remove/switch profiles

---
+++ lua/nvim-dap-runner/profiles.lua
@@ -0,0 +1,131 @@
+local util = require("lua.nvim-dap-runner.util")
+
+local M = {}
+
+--[[
+A profile contains the path to the binary and a name for the profile:
+{
+    name = "profile_name",
+    path = "/path/to/binary"
+}
+--]]
+local all_profiles = {}
+local active_profile = nil
+
+function M.is_profile_valid(profile_name)
+    assert(profile_name ~= nil and profile_name ~= "", "Profile name is nil or empty")
+    assert(M.profile_exists(profile_name), "Profile does not exist: " .. profile_name)
+
+    local function valid_check(test_str)
+        return test_str ~= nil and test_str ~= ""
+    end
+
+    local profile = M.get_profile(profile_name)
+    return valid_check(profile.name) and valid_check(profile.path)
+end
+
+function M.profile_exists(profile_name)
+    assert(profile_name ~= nil and profile_name ~= "", "Profile name is nil or empty")
+    return all_profiles[profile_name] ~= nil
+end
+
+function M.set_active_profile(profile_name)
+    assert(profile_name ~= nil and profile_name ~= "", "Profile name is nil or empty")
+    assert(M.profile_exists(profile_name), "Profile does not exist: " .. profile_name)
+    assert(M.is_profile_valid(profile_name), "Profile is not valid: " .. profile_name)
+    assert(active_profile ~= profile_name, "Active profile already set: " .. profile_name)
+
+    active_profile = profile_name
+
+    assert(M.get_active_profile() == M.get_profile(active_profile), "Active profile was not set correctly")
+    assert(M.is_profile_valid(active_profile), "Profile is not valid: " .. active_profile)
+end
+
+function M.get_active_profile()
+    return M.get_profile(active_profile)
+end
+
+function M.get_profile(profile_name)
+    assert(profile_name ~= nil and profile_name ~= "", "Profile name is nil or empty")
+    return all_profiles[profile_name]
+end
+
+function M.create_profiles(names, paths, profile_to_activate)
+    assert(names ~= nil and #names > 0, "Names is nil or empty")
+    assert(paths ~= nil and #paths > 0, "Paths is nil or empty")
+    assert(#paths == #names, "Paths and names are not the same length")
+
+    for i, path in ipairs(paths) do
+        assert(names[i] ~= nil and names[i] ~= "", "Name is nil or empty")
+        assert(path ~= nil and path ~= "", "Path is nil or empty")
+
+        M.create_profile(path, names[i], names[i] == profile_to_activate)
+    end
+end
+
+function M.create_profile(path, name, activate)
+    assert(path ~= nil and path ~= "", "Path is nil or empty")
+    assert(name ~= nil and name ~= "", "Name is nil or empty")
+    assert(not M.profile_exists(name), "Profile already exists: " .. name)
+
+    local prev_tot_profiles = util.table_len(all_profiles)
+    if prev_tot_profiles == 0 then
+        activate = true
+    end
+
+    all_profiles[name] = {
+        name = name,
+        path = path
+    }
+
+    if activate then
+        M.set_active_profile(name)
+    end
+
+    assert(prev_tot_profiles + 1 == util.table_len(all_profiles), "Profile was not added to all_profiles")
+    assert(M.profile_exists(name), "Profile does not exist after trying to create it: " .. name)
+    assert((activate and M.get_active_profile().name == name) or (not activate and active_profile ~= name),
+        "Active profile was not set correctly")
+end
+
+function M.delete_profiles(profile_names)
+    assert(profile_names ~= nil and #profile_names > 0, "Profile names is nil or empty")
+
+    local prev_tot_profiles = util.table_len(all_profiles)
+    for _, name in ipairs(profile_names) do
+        assert(name ~= nil and name ~= "", "Name is nil or empty")
+        assert(M.profile_exists(name), "Profile does not exist: " .. name)
+
+        M.delete_profile(name)
+    end
+
+    assert(prev_tot_profiles - #profile_names == util.table_len(all_profiles), "Profiles were not deleted")
+end
+
+function M.delete_profile(profile_name)
+    assert(profile_name ~= nil and profile_name ~= "", "Profile name is nil or empty")
+    assert(M.profile_exists(profile_name), "Profile does not exist: " .. profile_name)
+
+    local prev_tot_profiles = util.table_len(all_profiles)
+    all_profiles[profile_name] = nil
+    if active_profile == profile_name then
+        active_profile = nil
+    end
+
+    assert(prev_tot_profiles - 1 == util.table_len(all_profiles), "Profile was not deleted from all_profiles")
+    assert(not M.profile_exists(profile_name), "Profile still exists after trying to delete it: " .. profile_name)
+    assert(active_profile ~= profile_name, "Active profile was not unset")
+end
+
+function M.delete_all_profiles()
+    all_profiles = {}
+    active_profile = nil
+
+    -- TODO: Update the Toml file
+end
+
+function M.get_num_profiles()
+    return util.table_len(all_profiles)
+end
+
+return M
---
+++ tests/profiles_spec.lua
@@ -0,0 +1,161 @@
+local profiles = require("lua.nvim-dap-runner.profiles")
+
+describe("Profiles", function()
+    it("Create a new profile", function()
+        local name = "profile"
+        local path = "path/to/binary"
+        profiles.create_profile(path, name, true)
+        local profile = profiles.get_profile(name)
+        local active_profile = profiles.get_active_profile()
+        profiles.create_profile(path .. "2", name .. "2")
+
+        assert.truthy(profile ~= nil)
+        assert.truthy(active_profile ~= nil)
+        assert.same(profile.name, name)
+        assert.same(profile, active_profile)
+        assert.is_not.same(profile, profiles.get_profile(name .. "2"))
+
+        profiles.delete_all_profiles()
+    end)
+    it("Create a batch of profiles and activate one", function()
+        local names = { "profile1", "profile2", "profile3" }
+        local paths = { "path/to/binary", "path/to/binary2", "path/to/binary3" }
+        local active_profile_name = names[2]
+        profiles.create_profiles(names, paths, active_profile_name)
+
+        for i, _ in ipairs(paths) do
+            assert.truthy(profiles.is_profile_valid(names[i]))
+        end
+
+        assert.truthy(profiles.get_active_profile().name == active_profile_name)
+
+        profiles.delete_all_profiles()
+    end)
+    it("Switch between profiles", function()
+        local names = { "profile1", "profile2", "profile3" }
+        local paths = { "path/to/binary", "path/to/binary2", "path/to/binary3" }
+        profiles.create_profiles(names, paths)
+
+        for i, _ in ipairs(paths) do
+            assert.truthy(profiles.is_profile_valid(names[i]))
+        end
+
+        for i = #names, 1, -1 do
+            profiles.set_active_profile(names[i])
+            assert.truthy(profiles.get_active_profile().name == names[i])
+        end
+
+        assert.truthy(profiles.get_active_profile().name == names[1])
+
+        profiles.delete_all_profiles()
+    end)
+    it("Get the active profile", function()
+        local name = "profile1"
+        profiles.create_profile("path/to/binary", name, true)
+        local active = profiles.get_active_profile()
+        assert.truthy(active ~= nil)
+        assert.truthy(active.name == name)
+        assert.truthy(profiles.is_profile_valid(name))
+
+        profiles.delete_all_profiles()
+    end)
+    it("Getters return the same value", function()
+        local name = "profile1"
+        profiles.create_profile("path/to/binary", name, true)
+        local profile = profiles.get_profile(name)
+        local active = profiles.get_active_profile()
+        assert.same(profile, active)
+
+        profiles.delete_all_profiles()
+    end)
+    it("Profile exists behaves as expected", function()
+        local profiles_to_create = {
+            {
+                name = "profile1",
+                path = "path/to/binary"
+            },
+            {
+                name = "profile2",
+                path = "path/to/binary2"
+            },
+            {
+                name = "profile3",
+                path = "path/to/binary3"
+            }
+        }
+
+        local invalid_profile_names = {
+            "This is not a profile",
+            nil,
+            "Not this one either",
+        }
+
+        for _, profile in ipairs(profiles_to_create) do
+            profiles.create_profile(profile.path, profile.name)
+            assert.truthy(profiles.profile_exists(profile.name))
+        end
+
+        for _, name in ipairs(invalid_profile_names) do
+            assert.falsy(profiles.profile_exists(name))
+        end
+
+        profiles.delete_all_profiles()
+    end)
+    it("Delete profiles", function()
+        local profiles_to_create = {
+            {
+                name = "profile1",
+                path = "path/to/binary"
+            },
+            {
+                name = "profile2",
+                path = "path/to/binary2"
+            },
+            {
+                name = "profile3",
+                path = "path/to/binary3"
+            }
+        }
+
+        local function create_profiles()
+            for _, profile in ipairs(profiles_to_create) do
+                profiles.create_profile(profile.path, profile.name)
+            end
+        end
+
+        local function test_profiles_exist()
+            for _, profile in ipairs(profiles_to_create) do
+                assert.truthy(profiles.profile_exists(profile.name))
+            end
+        end
+
+        local function test_profiles_do_not_exist()
+            for _, profile in ipairs(profiles_to_create) do
+                assert.falsy(profiles.profile_exists(profile.name))
+            end
+        end
+
+        create_profiles()
+        test_profiles_exist()
+        for _, profile in ipairs(profiles_to_create) do
+            profiles.delete_profile(profile.name)
+        end
+        test_profiles_do_not_exist()
+
+        create_profiles()
+        test_profiles_exist()
+        local profiles_to_delete = {}
+        for _, profile in ipairs(profiles_to_create) do
+            table.insert(profiles_to_delete, profile.name)
+        end
+        profiles.delete_profiles(profiles_to_delete)
+        test_profiles_do_not_exist()
+
+        create_profiles()
+        test_profiles_exist()
+        profiles.delete_all_profiles()
+        test_profiles_do_not_exist()
+
+        assert.truthy(profiles.get_num_profiles() == 0)
+    end)
+end)
