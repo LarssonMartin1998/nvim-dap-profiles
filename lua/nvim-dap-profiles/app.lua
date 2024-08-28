0518078cdd5aae9309307f91c5f339a240084023
Martin Larsson <larssonmartin1998@gmail.com>
2024-08-28 23:46:39 +0200

    Fix bug which wouldnt run the "original_run" function when a profile exists

--- lua/nvim-dap-profiles/app.lua
+++ lua/nvim-dap-profiles/app.lua
@@ -7,23 +7,22 @@ local M = {}
 local function intercept_run_fn_and_insert_program()
     local original_run = dap.run
     dap.run = function(config, opts)
-        if not profiles.is_active_profile_set() then
-            original_run(config, opts)
-            return
+        if profiles.is_active_profile_set() then
+            local profile = profiles.get_active_profile()
+            local cwd = vim.fn.getcwd()
+            if cwd:sub(-1) ~= "/" then
+                cwd = cwd .. "/"
+            end
+
+            local profile_path = profile.path
+            if profile_path:sub(1, 1) == "/" then
+                profile_path = profile_path:sub(2)
+            end
+
+            config.program = cwd .. profile_path
         end
 
-        local profile = profiles.get_active_profile()
-        local cwd = vim.fn.getcwd()
-        if cwd:sub(-1) ~= "/" then
-            cwd = cwd .. "/"
-        end
-
-        local profile_path = profile.path
-        if profile_path:sub(1, 1) == "/" then
-            profile_path = profile_path:sub(2)
-        end
-
-        config.program = cwd .. profile_path
+        original_run(config, opts)
     end
 end
 
